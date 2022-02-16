5efc20ed1072122a394a6608d5628a69453c414b
André Arns <andreluisarns@gmail.com>
2022-02-16 15:21:01 -0300

    Update skins data

--- NanoBusiness/Service/FakeDatabase/SkinsSingleton.swift
+++ NanoBusiness/Service/FakeDatabase/SkinsSingleton.swift
@@ -7,8 +7,8 @@ final class SkinsSingleton {
     private init() {
         skins = [
             Skin(spriteImageName: "player-1", name: "Eve Rise", description: "An ascending climber", coinCost: nil, distanceRequired: nil, isLocked: false),
-            Skin(spriteImageName: "playerRobot", name: "Crazy Robot", description: "A chinese robot with a purpose.", coinCost: 5000, distanceRequired: nil, isLocked: true),
-            Skin(spriteImageName: "playerSnowman", name: "Snowman", description: "Everest is not cold for him.", coinCost: 1000, distanceRequired: 10000, isLocked: true)
+            Skin(spriteImageName: "playerRobot", name: "SKP-N0W", description: "Nobody can track them here", coinCost: 5000, distanceRequired: nil, isLocked: true),
+            Skin(spriteImageName: "playerSnowman", name: "Snowman", description: "The others melted", coinCost: 1000, distanceRequired: 10000, isLocked: true)
         ]
     }
 }
