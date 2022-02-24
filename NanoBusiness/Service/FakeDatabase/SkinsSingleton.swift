4bc970d18356365d021f040f722d5a0a1c547a99
André Arns <andreluisarns@gmail.com>
2022-02-24 08:16:13 -0300

    Bug fix

--- NanoBusiness/Service/FakeDatabase/SkinsSingleton.swift
+++ NanoBusiness/Service/FakeDatabase/SkinsSingleton.swift
@@ -8,7 +8,8 @@ final class SkinsSingleton {
         skins = [
             Skin(spriteImageName: "player-1", name: "Eve Rise", description: "An ascending climber", coinCost: nil, distanceRequired: nil, isLocked: false),
             Skin(spriteImageName: "playerSnowman", name: "Snowman", description: "The others melted", coinCost: 2000, distanceRequired: nil, isLocked: true),
-            Skin(spriteImageName: "playerRobot", name: "SKP-N0W", description: "Nobody can track them here", coinCost: 5000, distanceRequired: nil, isLocked: true)
+            Skin(spriteImageName: "playerRobot", name: "SKP-N0W", description: "Nobody can track them here", coinCost: 5000, distanceRequired: nil, isLocked: true),
+            Skin(spriteImageName: "playerYeti", name: "Yeti", description: "Going to buy groceries", coinCost: 7777, distanceRequired: nil, isLocked: true)
         ]
     }
 }
